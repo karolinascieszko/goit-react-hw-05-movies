bbd2e26ffff27daa0e79301d38286f8838ad246e
Karolina Ścieszko <karolinascieszko@gmail.com>
2022-01-07 15:31:09 +0100

    delete duplicated props

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,8 +5,8 @@ import styles from './Navigation.module.css'
 const Navigation = () => {
   return (
     <div className={styles.Navigation}>
-      <NavLink className={styles.NavigationItem} to="/" activeclassname={styles.Active} className={styles.Home}>Home</NavLink>
-      <NavLink className={styles.NavigationItem} to='/movies' activeclassname={styles.Active} className={styles.Movies}>Movies</NavLink>
+      <NavLink  to="/" activeclassname={styles.Active} className={styles.Home}>Home</NavLink>
+      <NavLink  to='/movies' activeclassname={styles.Active} className={styles.Movies}>Movies</NavLink>
     </div>
   );
 };
